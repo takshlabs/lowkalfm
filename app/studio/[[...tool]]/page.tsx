a92856b44ce4aa0ed5c44097ba1de466ca8116c4
takshlabs <biswassaswat@gmail.com>
2026-08-24 21:54:21 +0530

    Export Sanity Studio route on Vercel

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -2,6 +2,10 @@ import { SanityStudio } from "@/components/SanityStudio";
 
 export const dynamic = "force-static";
 
+export function generateStaticParams() {
+  return [{ tool: [] }];
+}
+
 export default function StudioPage() {
   return <SanityStudio />;
 }
